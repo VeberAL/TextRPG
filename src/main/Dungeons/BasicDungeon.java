fcb4091c07637031efa01928fa43a012d868ae5f
VeberAL <aleo83@rambler.ru>
2021-07-08 01:47:36 +0300

    Подземелье (метод Открытие)

---
+++ src/main/Dungeons/BasicDungeon.java
@@ -0,0 +1,7 @@
+package main.Dungeons;
+import main.Party;
+
+public interface BasicDungeon {
+    //Подземелье имеет свойство "открываться"
+   void open(Party party);
+}
---
+++ src/main/Dungeons/Dungeon.java
@@ -0,0 +1,21 @@
+package main.Dungeons;
+
+import main.Party;
+
+public class Dungeon implements BasicDungeon{
+    private boolean isOpened = false;
+
+    @Override
+    public void open(Party party)
+    {
+        //Проверка на текущее кол-во участников группы для открытия подземелья
+        if(party.getPartyMembers().length < 3) {
+            System.out.println("Недостаточно участников в группе (необходимо больше 3).");
+            isOpened = false;
+        }
+        else{
+            System.out.println("Добро пожаловать, герои!");
+            isOpened = true;
+        }
+    }
+}
