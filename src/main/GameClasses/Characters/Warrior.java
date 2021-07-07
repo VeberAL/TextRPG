a410dc0384b8b927319ab122c867954315f408ab
VeberAL <aleo83@rambler.ru>
2021-07-07 21:49:54 +0300

    Тип Warrior с начальными характеристиками

---
+++ src/main/GameClasses/Characters/Warrior.java
@@ -0,0 +1,17 @@
+package main.GameClasses.Characters;
+import main.GameClasses.AttackType;
+import main.GameClasses.CharacterClass;
+
+//Класс воин с конструктором начальных показателей характеристик
+public class Warrior extends CharacterClass {
+    public Warrior(String name) {
+        this.setLevel(1);
+        this.setHealthPoints(100);
+        this.setManaPoints(50);
+        this.setMaxHealthPoints(100);
+        this.setMaxManaPoints(50);
+        this.setName(name);
+        this.setAttackType(AttackType.PHYSICAL);
+        this.setAttackAmount(5);
+    }
+}
